9d92b81104676bae8dabb451470d4032d94fa071
zsavery <zsavery998@gmail.com>
2021-05-28 15:37:49 -0500

    init commit

---
+++ LList/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LList
+{
+    class Program
+    {
+        /*
+        take a lists of grades and a list of student names from a single course 
+        make a linked list.
+        */
+        static void Main(string[] args)
+        {
+            // List of CS 348 students
+            var studentNames = new List<string> 
+                {"Amy", "Billy", "Carl"};
+            // List of grades for CS 348
+            var studentGrades = new List<int> 
+                {85, 74, 89};
+
+            var course = new LinkedList<CS_348>();
+
+            for(int i = 0; i < studentNames.Count; i++)
+            {
+                var student = new CS_348(str: studentNames[i], num: studentGrades[i]);
+                course.AddLast(student);
+            }
+            Console.WriteLine("Number of students: {0}", course.Count);
+            foreach(CS_348 pupil in course)
+            {
+                pupil.Read();
+            }
+        } 
+    }
+}
+
+class CS_348
+{
+    string name = "";
+    int grade = 0;
+    public CS_348(string str, int num)
+    {
+        name = str;
+        grade = num;
+    }
+    public CS_348(int num)
+    {
+        grade = num;
+    }
+    public CS_348(string str)
+    {
+        name = str;
+    }
+    public void Read()
+    {
+        Console.WriteLine("Name: {0}\nGrade: {1}", name, grade);
+        
+    }
+}
