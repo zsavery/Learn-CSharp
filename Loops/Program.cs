7ead54aec113023b7463b6e0a6146ac6e0d150e3
zsavery <zsavery998@gmail.com>
2021-05-28 01:44:36 -0500

    initial commit 5/28/21

---
+++ Loops/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Loops
+{
+    class Program
+    {
+        /*
+        Make a program that contains a list of students going on a weekend field trip<Project Sdk="Microsoft.NET.Sdk">
+        Students should have
+            - first name
+            - last name
+            - bus seat number 
+            - group number
+                - Teacher/Chaperone 
+                - room number
+            - may have allergies
+
+        */
+        static void addStudent(ref List<FieldTripStudent> lst )
+        {
+            // Create class object
+            var newStudent = new FieldTripStudent();
+            // Get student first and last name
+            Console.Write("\nEnter first name: ");
+            newStudent.firstName = Console.Read().ToString();
+            Console.Write("\nEnter last name: ");
+            newStudent.lastName = Console.Read().ToString();
+            // Seat number for bus
+            Console.Write("\nEnter bus seat number: ");
+            newStudent.seatNumber = Console.Read();
+            // Group number
+            Console.Write("\nEnter group number: ");
+            newStudent.group.number = console.Read();
+            // Hotel Room Number
+            Console.Write("\nEnter full name of group chaparone: ");
+            newStudent.group.guideName = console.Read();
+            // 
+            Console.Write("\nEnter hotel room number: ");
+            newStudent.group.roomNumber = console.Read();
+            
+            lst.Add(newStudent);
+
+            return;
+        }
+        static void Main(string[] args)
+        {
+            bool keepGoing = true;
+            var fieldTrip = new List<FieldTripStudent>();
+            do
+            {
+                Console.Write("Do you wish to add a new student to the field trip list?\nEnter Yes or No: ");
+                var selection = Console.Read();
+                if(selection.ToString().ToLower() == "no")
+                {
+                    keepGoing = false;
+                }
+                else if(selection.ToString().ToLower() == "yes")
+                {
+                    addStudent();
+                }
+                else
+                {
+                    console.WriteLine("\nInvalid selection!");
+                    
+                }
+
+            }while(keepGoing == true);
+    
+        }
+    }
+
+    
+}
+
+class FieldTripStudent
+{
+    public string firstName;
+    public string lastName;
+    public int seatNumber;
+    public Group group; 
+
+}
+
+struct Group
+{
+    int number;
+    string guideName;
+    int roomNumber;
+}
---
+++ Sample/Sample/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Prompt(ref string nameField, ref string ageField,
+            ref string locationField)
+        {
+            // Get user name 
+            Console.Write("What is your first name? -> ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What is your last name? -> ");
+            string lastName = Console.ReadLine();
+
+            // Concatinate full name
+            nameField = string.Concat(firstName, lastName);
+
+            // Get user age
+            Console.Write("How old are you? -> ");
+            // Convert string to int of 16 bits
+            ageField = Console.ReadLine();
+
+            // Get location of origin
+            Console.Write("What state are you from? -> ");
+            locationField = Console.ReadLine();
+            Console.WriteLine();
+        }
+
+        static void survey1(string[] responsesField, string nameField,
+            string ageField, string locationField)
+        {
+            // Survey Output
+            Console.WriteLine(responsesField[0] + nameField);
+            Console.WriteLine(responsesField[1] + ageField);
+            Console.WriteLine(responsesField[2] + locationField);
+            return;
+        }
+
+        static void survey2(string[] responsesField, string nameField,
+            string ageField, string locationField)
+        {
+            // Survey Output 2
+            Console.WriteLine("{0}{1}", responsesField[0], nameField);
+            Console.WriteLine("{0}{1}", responsesField[1], ageField);
+            Console.WriteLine("{0}{1}", responsesField[2], locationField);
+        }
+
+
+        static void survey3(string[] responsesField, string nameField,
+            string ageField, string locationField)
+        {
+            // Survey Output 3
+            Console.WriteLine("{0}{1}\n{2}{3}\n{4}{5}", responsesField[0], nameField,
+                responsesField[1], ageField, responsesField[2], locationField);
+        }
+
+        static void survey4(string[] result)
+        {
+            // Survey Output 4
+            Console.WriteLine();
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result[1]);
+            Console.WriteLine(result[2]);
+        }
+
+        static void Main(string[] args)
+        {
+            // Full name
+            string name = string.Empty;
+
+            // Get user age
+            string age = string.Empty;
+
+            // Get location of origin
+            string location = string.Empty;
+
+            Prompt(nameField: ref name, ageField: ref age,
+                locationField: ref location);
+
+            string[] responses = { "Name: ", "Age: ", "Location: " };
+            ///survey1(responses, name, age, location);
+            //survey2(responses, name, age, location);
+            //survey3(responses, name, age, location);
+
+            string[] result = new string[3];
+            result[0] = $"{responses[0]}{name}";
+            result[1] = $"{responses[1]}{age}";
+            result[2] = $"{responses[2]}{location}";
+            survey4(result: result);
+
+            if (location.ToLower() == "texas" || location.ToLower() == "tx")
+            {
+                Console.WriteLine("*Computer points gun at head*");
+                Console.Write("Dallas Cowboys or Houston Texans?: ");
+                string lifeDecicion = Console.ReadLine();
+                if (lifeDecicion.ToLower() == "dallas cowboys" ||
+                    lifeDecicion.ToLower() == "cowboys")
+                {
+                    Console.WriteLine("*Computer Shoots Gun*\nBang!!\n" +
+                        "Don't blame me for my programming.");
+                }
+                else
+                {
+                    Console.WriteLine("*Computer puts gun away*\n" +
+                        "I'll be watching you!");
+                }
+            }
+        }
+    }
+}
